ab139dcc62e36277285811fdc36dc247e9c30e39
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-05-01 17:54:47 +0800

    fill json data into template

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+function replaceTemplate(temp, item) {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, item.productName);
+  output = output.replace(/{%IMAGE%}/g, item.image);
+  output = output.replace(/{%PRICE%}/g, item.price);
+  output = output.replace(/{%FROM%}/g, item.from);
+  output = output.replace(/{%NUTRIENTS%}/g, item.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, item.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, item.description);
+  output = output.replace(/{%ID%}/g, item.id);
+
+  if (!item.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return output;
+}
 const response = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
 const responseObj = JSON.parse(response);
 
 //FILE
@@ -27,18 +57,34 @@ const responseObj = JSON.parse(response);
 // });
 
 //SERVER
+
 const server = http.createServer((req, res) => {
   const pathURL = req.url;
+
+  //HOME PAGE
   if ((pathURL === "/") | (pathURL === "/overview")) {
-    res.end("welcome to overview page");
-  } else if (pathURL === "/product") {
+    res.writeHead(200, { "content-type": "text/html" });
+    const cardsHTML = responseObj
+      .map((item) => replaceTemplate(templateCard, item))
+      .join("");
+
+    console.log(cardsHTML);
+    const output = templateOverview.replace("%PRODUCT_CARDS%", cardsHTML);
+    res.end(output);
+  }
+  //PRODUCT PAGE
+  else if (pathURL === "/product") {
     res.end("welcome to product page");
-  } else if (pathURL === "/api") {
+  }
+  //API
+  else if (pathURL === "/api") {
     res.writeHead(200, "request SUCCESSFUL", {
       "content-type": "application/json",
     });
     res.end(response);
-  } else {
+  }
+  //INVALID REQUEST
+  else {
     res.writeHead(404, "Invalid page", {
       "Content-type": "text/html",
       "my-own-header": "hello-robin",
