37f8e2ecfe252740ab0c2d0f8fa8fa08befdc64d
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-05-03 21:00:56 +0800

    yesterday code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
+const slugify = require('slugify')
 
 const response = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const templateProduct = fs.readFileSync(
